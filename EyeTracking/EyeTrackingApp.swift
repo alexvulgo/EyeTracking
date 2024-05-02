0031f84b82c3a80de0eb9f610079529884fd5bd5
alexvulgo <120048791+alexvulgo@users.noreply.github.com>
2024-05-02 19:30:05 +0200

    starting new Xcode project

---
+++ EyeTracking/EyeTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeTrackingApp.swift
+//  EyeTracking
+//
+//  Created by Alessandro Esposito Vulgo Gigante on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct EyeTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
